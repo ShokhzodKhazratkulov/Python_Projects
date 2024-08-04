83fee11ff87404fc4fe6f329f9436edea051fee5
Shokhzod <khazratkulovshokhzod@gmail.com>
2024-08-05 00:15:20 +0800

    new projects

---
+++ Small_projects_regarding_web_scraping_and_bs4/main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://imgdownloader.com/"
+
+print(float(23%3))
+
+
+
+
+
+
+# URL = "https://daryo.uz/en"
+# response = requests.get(URL)
+# web_url = response.text
+#
+# soup = BeautifulSoup(web_url, "html.parser")
+#
+# all_news = soup.find_all("h3", class_="is-title post-title limit-lines l-lines-3")
+#
+# for news in all_news:
+#     print(news.a.string)
+
